0dd0547cbde1b64bfdfa8282fadecf71faae5048
Eli Fry <fryjah07@gmail.com>
2022-08-22 21:24:52 -0400

    Updated to connect to cloud db

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
 });
 
 pool.on('connect', () => {
